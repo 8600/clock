eecb2284f62aae3dd90baa237e331feb0c7bce7c
蒲鸽 <my@owo.help>
2017-10-11 23:23:47 +0800

    start

---
+++ index.vue
@@ -0,0 +1,27 @@
+<template lang="pug">
+  <div>{{time}}</div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        time: '',
+        clock: null
+      }
+    },
+    mounted () {
+      function checkTmie(num) {
+        return num < 10 ? '0' + num : num
+      }
+      this.clock = setInterval(() => {
+        const myDate = new Date()
+        this.time = checkTmie(myDate.getHours()) + ':' + checkTmie(myDate.getMinutes()) + ':' + checkTmie(myDate.getSeconds())
+      }, 1000)
+    },
+    beforeDestroy () {
+      // 清除定时器
+      window.clearInterval(this.clock)
+    }
+  }
+</script>
